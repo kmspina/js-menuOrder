e8fa51f5c86393e1c7d4b3cf6a215dd1d916acaa
kmspina <kmspina@wustl.edu>
2016-04-18 18:39:30 -0500

    done

--- menu.js
+++ menu.js
@@ -1,15 +1,23 @@
 // begin with a blank/new order
 var order = '';
 
-
-// build a function to ask questions
-
-function question() {
-  // your code goes here
+// ask questions
+function question(questiontext) {
+  var answerInput = prompt(questiontext);
+  order += answerInput + ", ";
+  if ( answerInput.toUpperCase() === order ) {
+  }
+  console.log(" ordered " + order)
 }
 
-// call the QUESTION function with the order options
-question();
+question("White or Brown rice?", order);
+question("Black or pinto beans?", order);
+question("Type of meat", order);
+question("Mild, medium, or hot salsa?", order);
+question("Lettuce, cheese, sour cream?", order);
+question("Guac? (extra cost)", order);
+question("Chips? (extra cost)", order);
+question("Drink? (extra cost)", order);
 
 // output their order
 document.write("<p>You ordered: " + order + "<p>");
